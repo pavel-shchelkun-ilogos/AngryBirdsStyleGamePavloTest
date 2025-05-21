ae717ac1c7e98c6cc3b7257a513f9c4fc511dc76
Pavel Shchelkun <pavel.shchelkun@ilogos.biz>
2025-05-21 19:08:07 +0300

    Rename Services into ServiceLocator field

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -19,7 +19,7 @@ public class GameManager : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-	    Game.Services.Register(this);
+	    Game.ServiceLocator.Register(this);
 	    
         CurrentGameState = GameState.Start;
         slingshot.enabled = false;
